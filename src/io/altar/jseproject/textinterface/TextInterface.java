e0224c92a7820ce44fce957a59bc087571431f7a
a90machado <a90machado@gmail.com>
2018-11-15 17:27:35 +0000

    fim

--- src/io/altar/jseproject/textinterface/TextInterface.java
+++ src/io/altar/jseproject/textinterface/TextInterface.java
@@ -1,9 +1,13 @@
 package io.altar.jseproject.textinterface;
 import java.util.Scanner;
+
 import io.altar.jseproject.model.Product;
+import io.altar.jseproject.model.Shelf;
 
 public class TextInterface {
-
+	
+	
+	//-------------------------------------------------------------------------
 	public void userInterface() {
 		System.out.println("Por favor selecione uma das seguintes opções:");
 		System.out.println("1) Listar produtos");
@@ -11,8 +15,9 @@ public class TextInterface {
 		System.out.println("3) Sair");
 
 		Scanner scanner = new Scanner(System.in);
-		int userAnswer = scanner.nextInt();
-		scanner.close();
+		String toTry = scanner.nextLine();
+		int userAnswer = checkType(toTry,"Int") ? Integer.parseInt(toTry) : 4;
+		
 
 		switch (userAnswer) {
 		case 1:
@@ -23,6 +28,7 @@ public class TextInterface {
 			break;
 		case 3:
 			//System.exit(0);
+			System.out.println("<>===Software desligado com sucesso!===<>");
 			break;
 		default:
 			// TODO : Clear console
@@ -30,8 +36,11 @@ public class TextInterface {
 			userInterface();
 			break;
 		}
+		scanner.close();
 	}
-
+	//-------------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------------
 	private void userInterfaceShowProducts() {
 		//TODO LIstAGEM
 		System.out.println("Por favor selecione uma das seguintes opções:");
@@ -42,21 +51,40 @@ public class TextInterface {
 		System.out.println("5) Voltar ao menu anterior");
 
 		Scanner scanner = new Scanner(System.in);
-		int userAnswer = scanner.nextInt();
-		scanner.close();
+		String toTry = scanner.nextLine();
+		int userAnswer = checkType(toTry,"Int") ? Integer.parseInt(toTry) : 6;
+		
 		
 		switch (userAnswer) {
 		case 1:
-			//TODO VERIFICAR VARIAVEIS
 			System.out.println("Insira desconto:");
-			double discontPrice = scanner.nextDouble();
+			toTry = scanner.nextLine();
+			while (checkType(toTry, "Double")==false){
+				System.out.println("Valor introduzido invalido, por favor repita: ");
+				toTry = scanner.nextLine();
+			}
+			double discontPrice = Double.parseDouble(toTry);
+			
 			System.out.println("Insira iva:");
-			double iva = scanner.nextDouble();
+			toTry = scanner.nextLine();
+			while (checkType(toTry, "Double")==false){
+				System.out.println("Valor introduzido invalido, por favor repita: ");
+				toTry = scanner.nextLine();
+			}
+			double iva = Double.parseDouble(toTry);
+			
 			System.out.println("Insira pvp:");
-			double pvp = scanner.nextDouble();
-			//TODO list of shelfs and id auto
+			toTry = scanner.nextLine();
+			while (checkType(toTry, "Double")==false){
+				System.out.println("Valor introduzido invalido, por favor repita: ");
+				toTry = scanner.nextLine();
+			}
+			double pvp = Double.parseDouble(toTry);
+			
 			Product newProduct = new Product(discontPrice, iva, pvp);
-			//TODO PUSH
+			System.out.print("Produto adicionado com sucesso!");
+			userInterfaceShowProducts();
+			
 			break;
 		case 2:
 
@@ -76,9 +104,11 @@ public class TextInterface {
 			userInterfaceShowProducts();
 			break;
 		}	
-		
+		scanner.close();
 	}
-
+	//-------------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------------
 	private void userInterfaceShowShelfs() {
 		//TODO LIstAGEM
 		System.out.println("Por favor selecione uma das seguintes opções:");
@@ -89,9 +119,9 @@ public class TextInterface {
 		System.out.println("5) Voltar ao menu anterior");
 
 		Scanner scanner = new Scanner(System.in);
-		int userAnswer = scanner.nextInt();
-		scanner.close();
-
+		String toTry = scanner.nextLine();
+		int userAnswer = checkType(toTry,"Int") ? Integer.parseInt(toTry) : 6;
+		
 		switch (userAnswer) {
 		case 1:
 
@@ -114,6 +144,26 @@ public class TextInterface {
 			userInterfaceShowShelfs();
 			break;
 		}
+		scanner.close();
 	}
-
+	//-------------------------------------------------------------------------
+	//-------------------------------------------------------------------------
+	private boolean checkType(String value, String match){
+		try{
+			switch (match) {
+			case "Int":
+				Integer.parseInt(value);
+				break;
+			case "Double":
+				Double.parseDouble(value);
+				break;
+			default:
+				break;
+			}
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
 }
